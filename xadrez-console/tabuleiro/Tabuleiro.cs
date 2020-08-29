cc3fcd57423797753c3ca84c1cca3e59135dc85f
Angelo Carvalho <angelosuporteti@gmail.com>
2020-08-29 12:00:46 -0300

    Implementado melhoria usando sobrecarga e implementado excecao para validar s posicoes

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -13,15 +13,43 @@
             pecas = new Peca[linhas, colunas];
         }
 
-        public Peca peca(int linha, int coluna) //Método para acesso da peca individual à matriz, visto que o acesso externo à matriz foi bloquedo
+        public Peca peca(int linha, int coluna) 
         {
             return pecas[linha, coluna];
         }
-        //Esta classe deve ter um método para oferecer operações de colocar uma peca no tabuleiro
+
+        public Peca peca(Posicao pos) //<--sobrecarga do método Peca
+        {
+            return pecas[pos.linha, pos.coluna]; //<--Melhoria: vai retornar as peças nas posições
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+        
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;
-            p.posicao = pos; //<--onde a peça está(posição da peça) em relação em relação ao tabuleiro
+            p.posicao = pos; 
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos. linha < 0 || pos.linha > linhas || pos.coluna < 0 || pos.coluna > colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if ( !posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
