30bcd92aaef5a65e4df9137a6c01f8ef7c7bbd6d
Angelo Carvalho <angelosuporteti@gmail.com>
2020-09-07 11:21:48 -0300

    Implementando movimentos da torre

--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -11,5 +11,63 @@ namespace xadrez
         {
             return "T";
         }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor; //<--Pode mover se a posição for nula ou se estiver uma peca com cor diferente
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //mover acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while(tab.posicaoValida(pos) && podeMover(pos))
+            {
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor) //se encontrar uma peça e for com cor diferente, vai parar o while
+                {
+                    break; 
+                }
+                pos.linha = pos.linha - 1; //se não tiver peça no caminho conforme if acima, vai poder continuar movendo para cima
+            }
+
+            //mover abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor) 
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1; 
+            }
+
+            //mover para direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor) 
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1; 
+            }
+
+            //mover para esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor) 
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1; 
+            }
+
+            return mat;
+        }
     }
 }
