0a7efc7c153c44c1c3303d76267e4b602015a814
Angelo Carvalho <angelosuporteti@gmail.com>
2020-09-22 14:37:03 -0300

    Començando implementação de jogada especial roque grande

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -82,10 +82,10 @@ namespace xadrez
                 mat[pos.linha, pos.coluna] = true;
             }
 
-            //#jogadaespecial roque
+            // #jogadaespecial roque
             if (qteMovimentos == 0 && !partida.xeque)
             {
-                //#jogadaespecial roque pequeno
+                // #jogadaespecial roque pequeno
                 Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
                 if (testeTorreParaRoque(posT1))
                 {
@@ -97,7 +97,23 @@ namespace xadrez
                     }
                 }
 
-            }
+                
+                if (qteMovimentos == 0 && !partida.xeque)
+                {
+                    // #jogadaespecial roque grande
+                    Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                    if (testeTorreParaRoque(posT2))
+                    {
+                        Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                        Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                        Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                        if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                        {
+                            mat[posicao.linha, posicao.coluna - 2] = true;
+                        }
+                    }
+
+                }
 
             return mat;
         }
